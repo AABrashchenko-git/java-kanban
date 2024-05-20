0d365ee9a4bba4125170252924b00389faf3a3a3
Andrey Brashchenko <aab90@yandex.ru>
2024-05-20 22:59:31 +0300

    Создан базовый каркас приложения

---
+++ src/Task.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Task {
+    String name;
+    String description;
+    static int id;
+    Progress progress;
+
+    HashMap<Task, Integer> tasks;
+
+
+
+}
