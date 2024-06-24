a741deb826cecb51861b606a2428db202795993b
Andrey Brashchenko <aab90@yandex.ru>
2024-06-24 23:16:00 +0300

    Подготовить проект к началу выполнения ТЗ-6

--- src/ru/practicum/taskTracker/service/InMemoryHistoryManager.java
+++ src/ru/practicum/taskTracker/service/InMemoryHistoryManager.java
@@ -1,19 +1,16 @@
 package ru.practicum.taskTracker.service;
 
 import ru.practicum.taskTracker.model.*;
-
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private static final int HISTORY_LIMIT = 10;
-    //Спасибо за инфу про LinkedList и про особенности использования LinkedList и ArrayList, полезно!
     private final List<Task> historyList = new LinkedList<>();
 
     @Override
     public void add(Task task) {
         if (historyList.size() == HISTORY_LIMIT) {
-            historyList.remove(0);
+            historyList.removeFirst();
         }
         historyList.add(task);
     }
@@ -21,7 +18,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     //Получение истории просмотра задач
     @Override
     public List<Task> getHistory() {
-       return new LinkedList<>(historyList);
+       return new ArrayList<>(historyList);
     }
 
 }
